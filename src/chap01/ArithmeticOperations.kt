f89386544499b82b42f49952d6068e0c246d9074
yongseopK <kk0@kakao.com>
2024-05-30 11:26:50 +0900

    # 3. 산술연산

---
+++ src/chap01/ArithmeticOperations.kt
@@ -0,0 +1,17 @@
+package chap01
+
+import java.lang.Math.*
+
+fun main() {
+
+    println(+2)
+    println(-2)
+    println(2 + 3)
+    println(1 - 3)
+    println(3 * 4)
+    println(7 / 4)
+    println(7 % 4)
+    println(floorDiv(7, 4)) // 코틀린 1.5부터 사용가능 정수 나눗셈과 같은 기능임
+    println(floorMod(7, 4)) // 정수 나머지 연산과 같은 기능임
+
+}
